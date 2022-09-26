b777dcff13447c1e7a501ec3f3c0b4faeddcd8be
Major2000 <majormadobe@gmail.com>
2022-09-26 15:41:35 +0300

    running the exported functions of the `NTDLL.DLL`

--- syscallhookdetect.cpp
+++ syscallhookdetect.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <windows.h>
 #include <psapi.h>
+
+
+
+int main()
+{
+    PDWORD functionAddress = (PDWORD)0;
+	
+	// Get ntdll base address
+	HMODULE libraryBase = LoadLibraryA("ntdll");
+
+	PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)libraryBase;
+	PIMAGE_NT_HEADERS imageNTHeaders = (PIMAGE_NT_HEADERS)((DWORD_PTR)libraryBase + dosHeader->e_lfanew);
+
+	// Locate export address table
+	DWORD_PTR exportDirectoryRVA = imageNTHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
+	PIMAGE_EXPORT_DIRECTORY imageExportDirectory = (PIMAGE_EXPORT_DIRECTORY)((DWORD_PTR)libraryBase + exportDirectoryRVA);
+
+	// Offsets to list of exported functions and their names
+	PDWORD addresOfFunctionsRVA = (PDWORD)((DWORD_PTR)libraryBase + imageExportDirectory->AddressOfFunctions);
+	PDWORD addressOfNamesRVA = (PDWORD)((DWORD_PTR)libraryBase + imageExportDirectory->AddressOfNames);
+	PWORD addressOfNameOrdinalsRVA = (PWORD)((DWORD_PTR)libraryBase + imageExportDirectory->AddressOfNameOrdinals);
+}
